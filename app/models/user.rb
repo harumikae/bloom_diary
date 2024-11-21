605c7d6c18520fc5d3b417168bc935f450d1cb0f
はるみかえ <llharu3132hp@gmail.com>
2024-11-21 11:29:02 +0900

    nameに対して空白はだめ validation設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  #デバイスの機能
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  #バリデーション（データのルール）
+  validates :name, presence: true #空はダメだよ・エラー発生やDBに保存されなくなるよ
 end
