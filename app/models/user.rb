8f578618c7296e7c7b2cf0aabe646847ff08eb0c
はるみかえ <llharu3132hp@gmail.com>
2024-11-29 16:50:22 +0900

    nameとpasswordの登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   #デバイスの機能
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
   
   #バリデーション（データのルール）
   validates :name, presence: true #空はダメだよ・エラー発生やDBに保存されなくなるよ
